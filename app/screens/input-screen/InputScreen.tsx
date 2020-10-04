14b5119aa7e172b3cfef8fd3bcc56b458667516d
marte-gleditsch <marte.gleditsch@bekk.no>
2020-10-04 21:19:56 +0200

    Håndtering av tomt input

--- app/screens/input-screen/InputScreen.tsx
+++ app/screens/input-screen/InputScreen.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { NavigationProp } from '@react-navigation/native'
 import { checkForMatch } from '/Users/martegleditsch/LoveCalculator/app/api/index'
 import { setMatchResult } from '/Users/martegleditsch/LoveCalculator/app/db/match-results'
-import { InputField, NavigationButton, Logo } from '/Users/martegleditsch/LoveCalculator/app/components/index'
+import {
+  InputField,
+  NavigationButton,
+  Logo,
+  SmallTextBold,
+} from '/Users/martegleditsch/LoveCalculator/app/components/index'
+import { colors } from '/Users/martegleditsch/LoveCalculator/app/config/colors'
 
 type Props = {
   navigation: NavigationProp<any>
@@ -13,12 +19,18 @@ function InputScreen(props: Props) {
   const [firstName, setFirstName] = useState<string>('')
   const [secondName, setSecondName] = useState<string>('')
 
+  const [showInputErrormessage, setShowInputErrorMessage] = useState(false)
+
   async function onCheckMatchPressed() {
-    const result = await checkForMatch(firstName, secondName)
-    if (result) {
-      setMatchResult(result)
+    if (firstName.length > 0 && secondName.length > 0) {
+      const result = await checkForMatch(firstName, secondName)
+      if (result) {
+        setMatchResult(result)
+      }
+      props.navigation.navigate('Result')
+    } else {
+      setShowInputErrorMessage(true)
     }
-    props.navigation.navigate('Result')
   }
 
   return (
@@ -29,7 +41,10 @@ function InputScreen(props: Props) {
         <InputField
           autoFocus
           placeholder={'First person´s name'}
-          onTextChange={(text: string) => setFirstName(text)}
+          onTextChange={(text: string) => {
+            setShowInputErrorMessage(false)
+            setFirstName(text)
+          }}
           value={firstName}
           index={0}
         />
@@ -37,16 +52,36 @@ function InputScreen(props: Props) {
         <InputField
           autoFocus={false}
           placeholder={'Second person´s name'}
-          onTextChange={(text: string) => setSecondName(text)}
+          onTextChange={(text: string) => {
+            setShowInputErrorMessage(false)
+            setSecondName(text)
+          }}
           value={secondName}
           style={{ marginTop: 48 }}
           index={1}
         />
       </View>
 
-      <NavigationButton onPress={onCheckMatchPressed} text={'Check if it`s a match'} />
+      <View style={{ alignItems: 'center' }}>
+        {showInputErrormessage && (
+          <SmallTextBold style={styles.errorMessage}>
+            {'You must enter two names to check if it`s a match!'}
+          </SmallTextBold>
+        )}
+        <NavigationButton onPress={onCheckMatchPressed} text={'Check if it`s a match'} />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  errorMessage: {
+    color: colors.orange,
+    position: 'absolute',
+    bottom: 100,
+    textAlign: 'center',
+    maxWidth: 300,
+  },
+})
 
 export default InputScreen
